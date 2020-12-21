b2eab7968f1e804eb2107d2eacacac052c3223be
Neil iMac <nmastroianni@longbranch.k12.nj.us>
2020-12-21 15:10:09 -0500

    removing logger statement

--- Code.js
+++ Code.js
@@ -1,6 +1,5 @@
 // Code for this add-on is in separate files
 function getDeveloperKey() {
   let props = PropertiesService.getScriptProperties().getProperties();
-  Logger.log(props);
   return props.DEVELOPER_KEY;
 }
