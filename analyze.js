3c24a7f3d7eebc719d991572c3de8d25f2bbac9a
Neil iMac <nmastroianni@longbranch.k12.nj.us>
2021-06-04 14:56:16 -0400

    fixing rating column flexibility

--- analyze.js
+++ analyze.js
@@ -8,22 +8,38 @@ function createSheet(title, index) {
   return ssInfo;
 }
 
+/**
+ * Takes a positive integer and returns the corresponding column name.
+ * @param {number} num  The positive integer to convert to a column name.
+ * @return {string}  The column name.
+ */
+function toColumnName(num) {
+  for (var ret = "", a = 1, b = 26; (num -= a) >= 0; a = b, b *= 26) {
+    ret = String.fromCharCode(parseInt((num % b) / a) + 65) + ret;
+  }
+  return ret;
+}
+
 function buildAnalysisSheet() {
   let ssInfo = createSheet("analysis", 1);
   const ss = ssInfo.ssheet;
+  let sheets = ss.getSheets();
+  let sheetNames = sheets.map((sheet) => sheet.getName()); // to add ability to overwrite the analysis sheet in future version.
+
   const sheet = ss.getSheetByName("analysis");
   const valRange = sheet.getRange(1, 2, 1, 5);
   valRange.merge();
   const dataSheet = ss.getSheetByName("studentResults");
+  const lastDataColumn = dataSheet.getLastColumn();
   const lastDataRow = dataSheet.getLastRow();
   const headerRow = dataSheet
     .getRange(1, 1, 1, dataSheet.getLastColumn())
     .getValues();
   const isFirstRating = (element) => element.includes("(Language)");
   const firstRatingIndex = headerRow[0].findIndex(isFirstRating);
-  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
   let dataHeadersRange = dataSheet.getRange(
-    `${alphabet.charAt(firstRatingIndex)}1:AE1`
+    `${toColumnName(firstRatingIndex + 1)}1:${toColumnName(lastDataColumn)}1`
   );
   let rule = SpreadsheetApp.newDataValidation()
     .requireValueInRange(dataHeadersRange)
